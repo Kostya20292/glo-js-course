9b372e1cadc3b088b26ddba7ae0ef0fd551dd98a
Konstantin Makarevich <mak.kostya2029@gmail.com>
2024-11-18 21:17:40 +0300

    feat: done lesson02

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = 'glo-js-course';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 100;
+const rollback = 11;
+const fullPrice = 250000;
+const adaptive = true;
 
-alert('Hello world');
+// alert('Hello world');
 
 console.log('Hello world');
+
+console.log(`Title type: ${typeof title}`);
+console.log(`FullPrice type: ${typeof fullPrice}`);
+console.log(`Adaptive type: ${typeof adaptive}`);
+console.log(`Screens length: ${screens.length}`);
+console.log(
+    `Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`
+);
+console.log(
+    `Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`
+);
+console.log(screens.toLowerCase().split(','));
+console.log(fullPrice * (rollback / 100));
