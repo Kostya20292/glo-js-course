e32f75cf24da5538f22f94440d6b35c6bd14b0b9
Konstantin Makarevich <mak.kostya2029@gmail.com>
2024-11-24 01:34:19 +0300

    feat: done lesson03-hard

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
-const title = prompt('Как называется ваш проект?');
-const screens = prompt('Какие типы экранов нужно разработать?');
-const screenPrice = +prompt('Сколько будет стоить данная работа?');
-const rollback = 11;
-const adaptive = confirm('Нужен ли адаптив на сайте?');
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = +prompt('Сколько это будет стоить?');
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice2 = +prompt('Сколько это будет стоить?');
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+const lang = prompt("Введите язык ('ru' или 'en')").toLowerCase();
+const days = {
+    ru: [
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота',
+        'Воскресенье',
+    ],
+    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+};
 
-console.log('title', title);
-
-console.log('screens', screens);
-
-console.log('screenPrice', screenPrice);
-
-console.log('adaptive', adaptive);
-
-console.log('service1', service1);
-
-console.log('servicePrice1', servicePrice1);
-
-console.log('service2', service2);
-
-console.log('servicePrice2', servicePrice2);
-
-console.log('fullPrice', fullPrice);
-
-console.log(servicePercentPrice);
-
-if (fullPrice >= 30000) {
-    console.log('Даем скидку в 10%');
-} else if (fullPrice >= 15000 && fullPrice <= 30000) {
-    console.log('Даем скидку в 5%');
-} else if (fullPrice <= 15000 && fullPrice >= 0) {
-    console.log('Скидка не предусмотрена');
-} else if (fullPrice < 0) {
-    console.log('Что то пошло не так');
+if (lang === 'ru') {
+    console.log(
+        'Понедельник, Вторник, Среда, Четверг, Пятница, Сууббота, Воскресенье'
+    );
+} else {
+    console.log(
+        'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
+    );
 }
+
+switch (lang) {
+    case 'ru':
+        console.log(
+            'Понедельник, Вторник, Среда, Четверг, Пятница, Сууббота, Воскресенье'
+        );
+        break;
+    case 'en':
+        console.log(
+            'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
+        );
+        break;
+}
+
+console.log(days[lang]);
