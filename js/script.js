b6a926844cb61ebc78051882b47cfab5749b676f
Konstantin Makarevich <mak.kostya2029@gmail.com>
2024-12-18 00:30:26 +0300

    feat: done lesson10-hard

--- js/script.js
+++ js/script.js
@@ -1,109 +1,18 @@
 'use strict';
 
-const title = document.getElementById('title');
-const handlerBtn = document.querySelectorAll('.handler_btn');
-const screenBtn = document.querySelector('.screen-btn');
-const otherPersent = document.querySelectorAll('.other-items.percent');
-const otherNumber = document.querySelectorAll('.other-items.number');
-const inputRollback = document.querySelector('.rollback input');
-const rangeValue = document.querySelector('.rollback .range-value');
-const totalInputs = document.querySelectorAll('.total-input');
-let screens = document.querySelectorAll('.screen');
+const inputElement = document.querySelector('input');
+const buttonElement = document.querySelector('button');
+const listElement = document.querySelector('ul');
 
-const appData = {
-    title: '',
-    screens: [],
-    screenPrice: 0,
-    adaptive: true,
-    rollback: 10,
-    allServicePrices: 0,
-    fullPrice: 0,
-    servicePercentPrice: 0,
-    services: {},
-    asking: () => {
-        do {
-            appData.title = prompt('Как называется ваш проект?');
-        } while (!appData.isString(appData.title));
+buttonElement.addEventListener('click', () => {
+    const inputValue = inputElement.value.trim();
 
-        for (let i = 0; i < 2; i++) {
-            let name;
-            let price;
+    if (inputValue) {
+        const listItem = document.createElement('li');
 
-            do {
-                name = prompt('Какие типы экранов нужно разработать?');
-            } while (!appData.isString(name));
+        listItem.textContent = inputValue;
+        listElement.append(listItem);
 
-            do {
-                price = +prompt('Сколько будет стоить данная работа?');
-            } while (!appData.isNumber(price) || price === 0);
-
-            appData.screens.push({ id: i, name, price });
-        }
-
-        for (let i = 0; i < 2; i++) {
-            let name;
-            let servicePrice;
-
-            do {
-                name = prompt('Какой дополнительный тип услуги нужен?');
-            } while (!appData.isString(name));
-
-            do {
-                servicePrice = +prompt('Сколько это будет стоить?');
-            } while (!appData.isNumber(servicePrice) || servicePrice === 0);
-
-            appData.services[`${name}${i}`] = +servicePrice;
-        }
-
-        appData.adaptive = confirm('Нужен ли адаптив на сайте?');
-    },
-    addPrices: () => {
-        appData.screenPrice = appData.screens.reduce((sum, screen) => {
-            return sum + screen.price;
-        }, 0);
-
-        for (let key in appData.services) {
-            appData.allServicePrices += appData.services[key];
-        }
-    },
-    isNumber: (num) => !isNaN(parseFloat(num)) && isFinite(num),
-    isString: (str) =>
-        typeof str === 'string' && str.trim() !== '' && isNaN(Number(str)),
-    getRollbackMessage: (price) => {
-        if (price >= 30000) {
-            return 'Даем скидку в 10%';
-        } else if (price >= 15000 && price <= 30000) {
-            return 'Даем скидку в 5%';
-        } else if (price <= 15000 && price >= 0) {
-            return 'Скидка не предусмотрена';
-        } else if (price < 0) {
-            return 'Что то пошло не так';
-        }
-    },
-    getFullPrice: () =>
-        (appData.fullPrice = appData.screenPrice + appData.allServicePrices),
-    getTitle: () =>
-        (appData.title =
-            appData.title.trim().slice(0, 1).toUpperCase() +
-            appData.title.trim().slice(1).toLowerCase()),
-    getServicePercentPrices: () =>
-        (appData.servicePercentPrice = Math.ceil(
-            appData.fullPrice - appData.fullPrice * (appData.rollback / 100)
-        )),
-    start: () => {
-        appData.asking();
-        appData.addPrices();
-        appData.getFullPrice();
-        appData.getServicePercentPrices();
-        appData.getTitle();
-
-        appData.logger();
-    },
-    logger: () => {
-        console.log(appData.fullPrice);
-        console.log(appData.servicePercentPrice);
-        console.log(appData.screenPrice);
-    },
-};
-
-appData.start();
+        inputElement.value = '';
+    }
+});
