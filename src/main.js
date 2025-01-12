ea964059a7f17b2471dec95d8030c335910eb401
Konstantin Makarevich <mak.kostya2029@gmail.com>
2025-01-12 21:08:20 +0300

    feat: done first hard task in lesson19

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { timer } from './modules/timer';
-import { daysFunc } from './modules/days';
+const input = document.querySelector('input');
+const p = document.querySelector('p');
 
-timer('22 january 2025');
-daysFunc();
+const debounce = (func, delay) => {
+  let timer; // Переменная для хранения таймера
+
+  return (...args) => {
+    clearTimeout(timer); // Очищаем предыдущий таймер
+    timer = setTimeout(() => func(...args), delay); // Устанавливаем новый таймер
+  };
+};
+
+input.addEventListener(
+  'input',
+  debounce(() => {
+    p.textContent = input.value;
+  }, 300)
+);
