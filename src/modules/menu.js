292130b650b81c81948822f34081b91d234ab593
Konstantin Makarevich <mak.kostya2029@gmail.com>
2025-01-13 22:12:03 +0300

    feat: done lesson20 hard task

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,11 +3,28 @@ export const menu = () => {
   const menuBtn = document.querySelector('.menu');
   const closeBtn = menuElement.querySelector('.close-btn');
   const menuItems = menuElement.querySelectorAll('ul>li>a');
+  const scrollBtn = document.querySelector('main > a');
+  const scrollElement = document.getElementById(scrollBtn.hash.slice(1));
 
   const handleMenu = () => menuElement.classList.toggle('active-menu');
 
   menuBtn.addEventListener('click', handleMenu);
   closeBtn.addEventListener('click', handleMenu);
 
-  menuItems.forEach((menuItem) => menuItem.addEventListener('click', handleMenu));
+  menuItems.forEach((menuItem) =>
+    menuItem.addEventListener('click', (e) => {
+      const clickedElement = document.getElementById(menuItem.hash.slice(1));
+
+      clickedElement.scrollIntoView({ block: 'start', behavior: 'smooth' });
+
+      e.preventDefault();
+      handleMenu();
+    })
+  );
+
+  scrollBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    scrollElement.scrollIntoView({ block: 'start', behavior: 'smooth' });
+  });
 };
