ae553412954baee3f9a427bb1031888c444f8292
Konstantin Makarevich <mak.kostya2029@gmail.com>
2025-01-17 23:01:59 +0300

    feat: done lesson23

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { modal } from './modules/modal';
 import { inputValidation } from './modules/inputValidation';
 import { tabs } from './modules/tabs';
 import { scroll } from './modules/scroll';
+import { slider } from './modules/slider';
 
 timer('22 january 2025');
 menu();
@@ -11,3 +12,4 @@ modal();
 scroll();
 inputValidation();
 tabs();
+slider();
---
+++ src/modules/slider.js
@@ -0,0 +1,46 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+export const slider = () => {
+  const portfolioSlider = new Swiper('.portfolio .swiper', {
+    loop: true,
+    centeredSlides: true,
+    modules: [Autoplay, Navigation, Pagination],
+    // autoplay: {
+    //   delay: 2000,
+    //   disableOnInteraction: false,
+    // },
+    pagination: {
+      el: '.portfolio-dots',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '#arrow-right',
+      prevEl: '#arrow-left',
+    },
+  });
+
+  const companiesSlider = new Swiper('.companies-wrapper.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    modules: [Autoplay],
+    autoplay: {
+      delay: 2000,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
+};
